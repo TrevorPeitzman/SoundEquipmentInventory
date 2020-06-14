d94dc41e26b6eeffed75b89dd4865e94e40b9c1f
Trevor Peitzman <trevor.peitzman@gmail.com>
2020-06-13 19:25:42 -0700

    Added basic spreadsheet getter from article
    https://www.twilio.com/blog/2017/02/an-easy-way-to-read-and-write-to-a-google-spreadsheet-in-python.html

---
+++ spreadsheet.py
@@ -0,0 +1,21 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
+
+
+# use creds to create a client to interact with the Google Drive API
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+
+# Find a workbook by name and open the first sheet
+# Make sure you use the right name here.
+sheet = client.open("Legislators 2017").sheet1
+
+# Extract and print all of the values
+# list_of_hashes = sheet.get_all_records()
+
+
+
+pprint(col)
