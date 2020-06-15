6d51d2b38d29d0e67e1daa7f89cc25db135ad5ec
Trevor Peitzman <trevor.peitzman@gmail.com>
2020-06-14 22:40:35 -0700

    Use same sheet getter syntax everywhere

--- history.py
+++ history.py
@@ -9,7 +9,7 @@ creds = ServiceAccountCredentials.from_json_keyfile_name("sei_creds.json", scope
 
 client = gspread.authorize(creds)
 
-sheet = client.open("Inventory Backend").sheet1  # Open the Backend, look at sheet 1
+sheet = client.open("Inventory Backend").get_worksheet(0)  # Open the Backend, look at sheet 1
 
 
 def check_in(barcode: str):
