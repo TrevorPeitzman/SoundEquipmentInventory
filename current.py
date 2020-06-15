beb3ba12b2e6f9bccaeb72539ec542e815c74332
Trevor Peitzman <trevor.peitzman@gmail.com>
2020-06-14 22:41:34 -0700

    Added where_is and update_location functions

--- current.py
+++ current.py
@@ -3,7 +3,7 @@ import utils
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
-          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 
 creds = ServiceAccountCredentials.from_json_keyfile_name("sei_creds.json", scope)
 
@@ -12,13 +12,28 @@ client = gspread.authorize(creds)
 sheet = client.open("Inventory Backend").get_worksheet(1)  # Open the Backend, look at sheet 2
 
 
-def check_in(barcode: str):
+def add_entry(barcode: str, location: str):
     """ Update location of item barcode to 'SHOP' """
     # Log the current date and time and append row to inventory sheet
-    row = utils.datetimearray() + [ barcode, "SHOP" ]
+    row = utils.datetimearray() + [barcode, location]
     sheet.append_row(row)
 
 
-def whereis(barcode: str):
-    print("lol")
-    
+def where_is(barcode: str):
+    try:
+        cell = sheet.find(barcode)
+        location = sheet.cell(cell.row, cell.col + 1).value
+        return location
+
+    except:
+        return "Unable to locate " + barcode
+
+
+def update_location(barcode: str, location: str):
+    try:
+        cell = sheet.find(barcode)
+        sheet.update_cell(cell.row, cell.col + 1, location)
+
+    except:
+        print("Unable to locate " + barcode + ".")
+
